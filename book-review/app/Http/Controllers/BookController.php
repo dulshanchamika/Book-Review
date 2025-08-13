397510e31b374b6b2ec272864a66136783548cd7
Dulshan Chamika <dulshanchamika7@gmail.com>
2025-08-13 17:29:54 +0530

    finalized

--- book-review/app/Http/Controllers/BookController.php
+++ book-review/app/Http/Controllers/BookController.php
@@ -2,8 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Book;
+use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
@@ -25,9 +25,17 @@ class BookController extends Controller
             'popular_last_6months' => $books->popularLast6Months(),
             'highest_rated_last_month' => $books->highestRatedLastMonth(),
             'highest_rated_last_6months' => $books->highestRatedLast6Months(),
-            default => $books->latest()
+            default => $books->latest()->withAvgRating()->withReviewsCount()
         };
-        $books = $books->get();
+
+        $cacheKey = 'books:' . $filter . ':' . $title;
+        $books =
+            cache()->remember(
+                $cacheKey,
+                3600,
+                fn() =>
+                $books->get()
+            );
 
         return view('books.index', ['books' => $books]);
     }
@@ -51,9 +59,20 @@ class BookController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(int $id)
     {
-        //
+        $cacheKey = 'book:' . $id;
+
+        $book = cache()->remember(
+            $cacheKey,
+            3600,
+            fn() =>
+            Book::with([
+                'reviews' => fn($query) => $query->latest()
+            ])->withAvgRating()->withReviewsCount()->findOrFail($id)
+        );
+
+        return view('books.show', ['book' => $book]);
     }
 
     /**
--- book-review/app/Providers/AppServiceProvider.php
+++ book-review/app/Providers/AppServiceProvider.php
@@ -3,9 +3,12 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
 
 class AppServiceProvider extends ServiceProvider
 {
+
     /**
      * Register any application services.
      */
@@ -19,6 +22,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        RateLimiter::for('reviews', function ($request) {
+            return Limit::perHour(5)->by(optional($request->user())->id ?: $request->ip());
+        });
     }
 }
